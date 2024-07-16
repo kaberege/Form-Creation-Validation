1f75b12efd1c41f98adc1243492012d4ebc6ecac
kaberege <kabgnestor@gmail.com>
2024-07-16 11:39:39 +0200

    form validation completed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Form-Creation-Validation
+        "Deep Dive into JavaScript” project!
+  In this project,  a user registration form with validation and fetch user data from a public API is develeoped. T
---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        document.getElementById("username");
+        const username = document.getElementById("username").value.trim();
+        document.getElementById("email");
+        const email = document.getElementById("email").value.trim();
+        document.getElementById("password");
+        const password = document.getElementById("password").value.trim();
+        let isValid = true;
+        let messages = [];
+        if (username.length < 3) {
+            isValid = false;
+            messages.push("Username error: Input username is too short!");
+        }
+        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+            isValid = false;
+            messages.push("Email error: Email is incorrect!");
+        }
+        if (password.length < 8) {
+            isValid = false;
+            messages.push("Password error: Your Password is too short!");
+        }
+        feedbackDiv.style.display = "block";
+        if (isValid) {
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = "#28a745";
+        } else {
+            feedbackDiv.innerHTML = messages.join("<br>");
+            feedbackDiv.style.color = "#dc3545";
+        }
+    });
+});
